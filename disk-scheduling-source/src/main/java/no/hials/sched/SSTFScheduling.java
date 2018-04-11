3aa1a8af22c2301b63f243c056fd7d52a6f7d767
Unknown <olav.telseth94@hotmail.com>
2018-04-11 13:53:58 +0200

    Done

--- disk-scheduling-source/src/main/java/no/hials/sched/SSTFScheduling.java
+++ disk-scheduling-source/src/main/java/no/hials/sched/SSTFScheduling.java
@@ -11,38 +11,77 @@ import java.util.List;
  */
 public class SSTFScheduling extends DiskSchedulingAlgo {
 
-    private int movement; // Total head movement
+    private int movement = 0; // Total head movement
+    private ArrayList<Integer> takenDisk = new ArrayList<>();
+    private int closestNumberDifference;
+    private int prevClosestNumber = 0;
 
     @Override
     public int process(List<Integer> refList) {
 
-        Collections.sort(refList);
-
-        //this.movement = super.getHeadPosition();
+        this.movement = 0;
+        takenDisk.clear();
 
         if (refList == null ||  refList.isEmpty()) {
             // No references means "empty sequence" == no movement
             return 0;
         }
 
-        int someheadposint = super.getHeadPosition();
-
         for (Integer integer : refList) {
             if ((integer < 0) || (integer >= super.getCylinderCount()))
                 return -1;
 
-            this.movement = Math.abs(super.getHeadPosition() - refList.get(0)); //-39 -> 39
-            super.moveHead(integer);
+            //Get closest number
+            int closestNumber = getCylinderCount();
+            int closestNumberIndex = 0;
+            closestNumberDifference = getCylinderCount();
+            //System.out.println(closestNumber);
+            //System.out.println(super.getHeadPosition());
+            //System.out.println(refList.get(0));
+
+
+            for(int i = 0; i <= refList.size()-1; i++) {
+                if ((isIndexFree(i))) {
+                    //System.out.println("current Number: " + refList.get(i));
+                    //System.out.println("The closest: " + closestNumber);
+                    //System.out.println("The difference: " + closestNumberDifference);
+                    if (Math.abs(super.getHeadPosition() - refList.get(i)) < closestNumberDifference) {
+                        closestNumber = refList.get(i);
+                        closestNumberIndex = i;
+                        //System.out.println(closestNumber);
+                        closestNumberDifference = Math.abs(super.getHeadPosition() - refList.get(closestNumberIndex));
+                    }
+                }
+            }
+            takenDisk.add(closestNumberIndex);
+            //System.out.println("Choosen number: " + closestNumber);
+
+            //System.out.println("prevClosest: " + prevClosestNumber);
+
+            super.moveHead(closestNumber);
             /*movement += integer;*/
+            this.movement += closestNumberDifference;
+            prevClosestNumber = closestNumber;
+
+            //System.out.println("movement: " + movement);
         }
 
         // TODO - fill in your algorithm here
         // Detect which position to move to in each step and call
         // moveHead()
         // See FCFSScheduling as an example
-        movement = Math.abs(someheadposint - (refList.get(refList.size() - 1)));
 
         return this.movement;
+    }
+
+    private boolean isIndexFree(int i) {
+        boolean result = true;
+        for(Integer j : takenDisk) {
+            if(i == j) {
+                result = false;
+            }
+        }
+        return result;
     }
 
 }
