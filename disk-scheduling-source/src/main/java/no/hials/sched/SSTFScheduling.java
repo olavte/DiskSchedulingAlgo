ff3100c170edaba74c672425767ff3f72fc31471
Unknown <olav.telseth94@hotmail.com>
2018-04-11 11:38:52 +0200

    Done simple and error

--- disk-scheduling-source/src/main/java/no/hials/sched/SSTFScheduling.java
+++ disk-scheduling-source/src/main/java/no/hials/sched/SSTFScheduling.java
@@ -6,32 +6,32 @@ import java.util.List;
 /**
  * SSTF Disk scheduling algorithm (Shortest Seek Time First) Takes the job which
  * lies closest to the "seeker" Fill in your code in this class!
- *
+ * <p>
  * 2016-04-04
  */
 public class SSTFScheduling extends DiskSchedulingAlgo {
 
     private int movement; // Total head movement
-    
+
     @Override
     public int process(List<Integer> refList) {
-        
+
         Collections.sort(refList);
-        
+
         //this.movement = super.getHeadPosition();
 
-        if (refList == null) {
+        if (refList == null ||  refList.isEmpty()) {
             // No references means "empty sequence" == no movement
             return 0;
         }
-        
+
         int someheadposint = super.getHeadPosition();
-        
+
         for (Integer integer : refList) {
-            if ((integer < 0) || (integer > super.getCylinderCount())) 
+            if ((integer < 0) || (integer >= super.getCylinderCount()))
                 return -1;
-            
-            this.movement = Math.abs(super.getHeadPosition() - refList.get(0));
+
+            this.movement = Math.abs(super.getHeadPosition() - refList.get(0)); //-39 -> 39
             super.moveHead(integer);
             /*movement += integer;*/
         }
@@ -40,8 +40,8 @@ public class SSTFScheduling extends DiskSchedulingAlgo {
         // Detect which position to move to in each step and call
         // moveHead()
         // See FCFSScheduling as an example
-        movement = Math.abs(someheadposint - refList.get(refList.get(refList.size()-1)));
-        
+        movement = Math.abs(someheadposint - (refList.get(refList.size() - 1)));
+
         return this.movement;
     }
 
